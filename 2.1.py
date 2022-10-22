5cf361a2b15b2d9020d2487bc2f0c7589aa53b87
burgerkill098 <115003776+burgerkill098@users.noreply.github.com>
2022-10-22 15:56:40 +0700

    Add files via upload

---
+++ 2.1.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+print ("             mencari bilangan terbesar           ")
+print("______________________________________________________")
+
+bil1=int(input("masukkan bilangan pertama :"))
+bil2=int(input("masukkan bilangan ke dua :"))
+
+
+
+if (bil1>bil2):
+    print("bilangan terbesar adalah :",bil1)
+elif (bil2>bil1):
+     print("bilangan terbesar adalah :",bil2)
